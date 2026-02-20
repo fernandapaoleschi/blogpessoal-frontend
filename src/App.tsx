1db90c70c98317b4adba0a2b972475c020f80f19
Fernanda Paoleschi <fernandapaoleschi1@gmail.com>
2026-02-19 22:25:49 -0300

    Cadastrar e logar usuario

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
     return (
         <>
+        <AuthProvider>
             <BrowserRouter>
                 <Navbar />
                 <div className="min-h-[80vh]">
@@ -19,7 +21,9 @@ function App() {
                 </div>
                 <Footer />
             </BrowserRouter>
+            </AuthProvider>
         </>
+
 
     );
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,33 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert('O Usuário foi desconectado com sucesso!');
+    navigate('/');
+  }
+
   return (
     <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
-        <div className="container flex justify-between text-lg mx-8">
-<Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-            <div className="flex gap-4">
-                Postagem
-                Temas
-                Cadastrar tema
-                Perfil
-                Sair
-            </div>
+      <div className="container flex justify-between text-lg mx-8">
+        <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+        <div className="flex gap-4">
+          Postagem
+          Temas
+          Cadastrar tema
+          Perfil
+          <Link to='' onClick={logout} className="cursor-pointer">
+            Sair
+          </Link>
         </div>
+      </div>
     </div>
   )
 }
